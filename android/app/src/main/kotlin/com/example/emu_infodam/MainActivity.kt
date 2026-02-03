ec86129c084b56a64486cbfcebc0a76a9dd0f57c
Sam Zaccone <samrzaccone@gmail.com>
2026-02-03 11:36:20 -0500

    initial commit

---
+++ android/app/src/main/kotlin/com/example/emu_infodam/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.emu_infodam
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'EMU Information Dam',
+
+      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple), useMaterial3: false),
+      home: const Placeholder(),
+    );
+  }
+}
